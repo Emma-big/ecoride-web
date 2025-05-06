035904d54e7860b9c864681c1a74d37ce004f12a
Emmanuelle CADILHAC <emmanuelle.cadilhac@gmail.com>
2025-05-06 22:49:29 +0200

    Isoler bootstrap PHPUnit et pointer phpunit.xml dessus

--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,7 @@
 // 1) Autoload Composer
 require __DIR__ . '/vendor/autoload.php';
 
-// 2) Charger .env si présent
+// 2) Charger .env si présent (pour les variables d’environnement)
 if (file_exists(__DIR__ . '/.env')) {
     \Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
 }
-
-// IMPORTANT : on **n’inclut pas** public/index.php ici.
