1e05d77d4461cc4a93181bb108db9b383f9bc629
Emmanuelle CADILHAC <emmanuelle.cadilhac@gmail.com>
2025-05-06 22:51:47 +0200

    Ajout de bootstrap PHPUnit et mise à jour de phpunit.xml

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 // bootstrap.php
 
-// 1) Autoload Composer
+// 1) Chargement de l’autoloader Composer
 require __DIR__ . '/vendor/autoload.php';
 
-// 2) Charger .env si présent (pour les variables d’environnement)
+// 2) Chargement du fichier .env si présent
 if (file_exists(__DIR__ . '/.env')) {
     \Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
 }
