7664f2342f68e814da059193db9e9e0769530eee
Emmanuelle CADILHAC <emmanuelle.cadilhac@gmail.com>
2025-05-14 21:44:09 +0200

    Fix syntax MySQL: use KEY instead of INDEX in create_login_attempts

--- scripts/create_login_attempts.php
+++ scripts/create_login_attempts.php
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS login_attempts (
   id INT AUTO_INCREMENT PRIMARY KEY,
   ip_address VARCHAR(45) NOT NULL,
   attempted_at DATETIME NOT NULL,
-  INDEX(idx_ip_time) (ip_address, attempted_at)
+  KEY idx_ip_time (ip_address, attempted_at)
 );
 SQL;
 
